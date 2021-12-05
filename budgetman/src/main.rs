491af672105bffdf3af6f18a2835060e39030028
Spyros Roum <spyrosr1@gmail.com>
2021-12-05 13:16:54 +0200

    Add logging

--- budgetman/src/main.rs
+++ budgetman/src/main.rs
@@ -5,12 +5,30 @@ use std::{convert::Infallible, env};
 use {
     anyhow::Context,
     dotenv::dotenv,
+    tracing::{debug, dispatcher::SetGlobalDefaultError},
+    tracing_subscriber::EnvFilter,
     warp::{http::Uri, Filter, Rejection, Reply},
 };
+
+fn setup_logging() -> Result<(), SetGlobalDefaultError> {
+    let app_name = concat!(env!("CARGO_PKG_NAME"), "_", env!("CARGO_PKG_VERSION"));
+    debug!("Init Logger {}", app_name);
+    let rust_log = env::var("RUST_LOG").unwrap_or_else(|_| String::from("DEBUG"));
+
+    let subscriber = tracing_subscriber::fmt()
+        .with_env_filter(EnvFilter::new(rust_log))
+        .pretty()
+        .finish();
+
+    tracing::subscriber::set_global_default(subscriber)
+}
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     dotenv().ok();
+    if let Err(e) = setup_logging() {
+        eprintln!("[WARNING]: Failed to setup logging: {}", e);
+    }
 
     let db = crud::create_db().await?;
     if let Err(e) = crud::add_default_user(&db).await {
@@ -26,9 +44,16 @@ async fn main() -> anyhow::Result<()> {
 
     let routes = admin_lte.or(views).or(api).recover(handle_rejection);
 
-    let server = warp::serve(routes).run(([0, 0, 0, 0], 8000));
-    println!("Listening at http://0.0.0.0:8000");
-    server.await;
+    let routes_with_logs = routes.with(warp::trace::trace(|info| {
+        tracing::info_span!(
+            "request",
+            method = %info.method(),
+            path = %info.path()
+        )
+    }));
+    warp::serve(routes_with_logs)
+        .run(([0, 0, 0, 0], 8000))
+        .await;
 
     Ok(())
 }
