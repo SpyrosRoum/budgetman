a38be2980bfc3580053f04a7d6e899b5d459e24f
Spyros Roum <spyrosr1@gmail.com>
2022-01-02 14:43:39 +0200

    Fix broken migration
    How did this happen?

--- migrations/20211123124146_create_users.up.sql
+++ migrations/20211123124146_create_users.up.sql
@@ -1,9 +1,7 @@
 CREATE TABLE IF NOT EXISTS users
 (
-    id uuid PRIMARY KEY DEFAULT gen_random_uuid
-(
-),
+    id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
     username VARCHAR NOT NULL UNIQUE,
     password_hash VARCHAR NOT NULL, -- Argon2id algorithm
     "admin" BOOL NOT NULL DEFAULT false
-    );
+);
